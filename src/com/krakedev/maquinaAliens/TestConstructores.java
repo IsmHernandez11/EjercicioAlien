9925d1c7ff32e6e6b391a8ca7c393e2989b5a3bd
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-04-17 20:03:09 -0500

    ISHF: TestContructor

--- src/com/krakedev/maquinaAliens/TestConstructores.java
+++ src/com/krakedev/maquinaAliens/TestConstructores.java
@@ -1,14 +1,18 @@
 package com.krakedev.maquinaAliens;
 
 public class TestConstructores {
-
 	public static void main(String[] args) {
-		Alien alien1 = new Alien(7,"red");
-		Alien alien2 = new Alien(2,"green");
-		
-		alien1.imprimir();
-		alien2.imprimir();
 
-	}
+        Alien a1 = new Alien(2, "Verde");  
+        Alien a2 = new Alien(40, "Rojo");   
 
+        a1.imprimir();
+        a2.imprimir();
+
+        a1.agregarBrazos(2);
+        a1.agregarPiernas(2);
+        a1.agregarOjos(3);
+
+        a1.imprimir();
+    }
 }
